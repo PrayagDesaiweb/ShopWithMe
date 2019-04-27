3177fd5fecda397688e6b34e728c9cb53b745ec1
PrayagDesaiweb <prayagdesai24397@gmail.com>
2019-04-27 15:30:36 +0530

    Adding Pages viewmodels and Page Dto. Adding Database for pages.

---
+++ ShopWithMe/Models/ViewModels/Pages/PageVm.cs
@@ -0,0 +1,38 @@
+﻿using ShopWithMe.Models.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ShopWithMe.Models.ViewModels.Pages
+{
+    public class PageVm
+    {
+        public PageVm()
+        {
+
+        }
+        public PageVm(PageDTO row)
+        {
+            Id = row.Id;
+            Title = row.Title;
+            Slug = row.Slug;
+            Body = row.Body;
+            Sorting = row.Sorting;
+            HasSidebar = row.HasSidebar;
+
+        }
+
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public string Title { get; set; }
+        public string Slug { get; set; }
+        [Required]
+        [StringLength(int.MaxValue,MinimumLength = 3)]  
+        public string Body { get; set; }
+        public int Sorting { get; set; }
+        public bool HasSidebar { get; set; }
+    }
+}
